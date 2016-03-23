aaa5ce641ce10be1c64b6116e34f336b6dd175c8
Jackie Ng <jumpinjackie@gmail.com>
2016-03-23 22:19:08 +1100

    Take off and nuke the site from orbit

--- modules/home/js/controllers/home.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-    app.controller('HomeController', function ($scope, home,uiGmapGoogleMapApi) {
-        $scope.title = home.title;
-
-        $scope.map = { center: { latitude: -37.8602828, longitude: 145.079616 }, zoom: 8 };
-    });
-})(angular.module('track-chat.home'));
